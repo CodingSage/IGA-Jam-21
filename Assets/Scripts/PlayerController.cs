1a94b7fc19dc7c9f4e3a02c5a1c7fab3288c6ed8
schaub818 <schaub818@gmail.com>
2021-03-20 13:17:32 -0400

    Rewrote PlayerController to use Character Controller component

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,31 +4,102 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    new Rigidbody rigidbody;
+    //new Rigidbody rigidbody;
 
-    public float movementSpeed = 500.0f;
+    //public float movementSpeed = 100.0f;
+    //public float jumpSpeed = 100.0f;
 
-    private void Awake()
-    {
-        rigidbody = GetComponent<Rigidbody>();
-    }
+    //private bool jumping;
+    //private Vector3 targetVelocity;
 
-    // Start is called before the first frame update
+    //void Start()
+    //{
+    //    rigidbody = GetComponent<Rigidbody>();
+    //    jumping = false;
+    //    targetVelocity = Vector3.zero;
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    ProcessMove();
+
+    //    if (Input.GetButtonDown("Jump"))
+    //    {
+    //        ProcessJump();
+    //    }
+
+    //    //ApplyVelocity(targetVelocity);
+    //}
+
+    //private void ProcessMove()
+    //{
+    //    //targetVelocity.x = Input.GetAxisRaw("Horizontal");
+    //    //targetVelocity.z = Input.GetAxisRaw("Vertical");
+
+    //    float moveHorizontal = Input.GetAxis("Horizontal");
+    //    float moveVertical = Input.GetAxis("Vertical");
+
+    //    Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+    //    rigidbody.AddForce(movement);
+    //}
+
+    //private void ProcessJump()
+    //{
+    //    if (!jumping)
+    //    {
+    //        jumping = true;
+
+    //        rigidbody.AddForce(new Vector3(0.0f, jumpSpeed, 0.0f));
+    //    }
+    //}
+
+    //private void ApplyVelocity(Vector3 targetVelocity)
+    //{
+    //    rigidbody.velocity = (targetVelocity * movementSpeed) * Time.deltaTime;
+
+    //    rigidbody.velocity = new Vector3(
+    //            targetVelocity.x * movementSpeed * Time.deltaTime,
+    //            rigidbody.velocity.y,
+    //            targetVelocity.z * movementSpeed * Time.deltaTime);
+    //}
+
+    [SerializeField]
+    private float moveSpeed = 5.0f;
+
+    [SerializeField]
+    private float gravityAcceleration = 9.81f;
+    private float ySpeed;
+    private float fallTime;
+
+    private CharacterController controller;
+
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+        ySpeed = 0.0f;
+        fallTime = 0.0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 targetVelocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+        float hAxis = Input.GetAxis("Horizontal");
+        float vAxis = Input.GetAxis("Vertical");
 
-        Move(targetVelocity);
-    }
+        if (controller.isGrounded)
+        {
+            ySpeed = 0.0f;
+            fallTime = 0.0f;
+        }
+        else
+        {
+            fallTime += Time.deltaTime;
+            ySpeed -= gravityAcceleration * fallTime;
+        }
 
-    private void Move(Vector3 targetVelocity)
-    {
-        rigidbody.velocity = (targetVelocity * movementSpeed) * Time.deltaTime;
+        Vector3 direction = new Vector3(hAxis, ySpeed, vAxis);
+
+        controller.Move(direction * moveSpeed * Time.deltaTime);
     }
 }
