ae2ed10f40a2061290a6cc0029f6e50127121608
Vinayak Karuppasamy <vinayak.rain@gmail.com>
2021-03-20 16:57:59 -0400

    enabled level menu on esc

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -5,6 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour
 {
+    public GameObject levelMenuPanel;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            levelMenuPanel.SetActive(!levelMenuPanel.active);
+        }
+    }
+
     public void ReplayLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
