1b142b7c9fdb52fff8094a270843f398b75e79d1
Vinayak Karuppasamy <vinayak.rain@gmail.com>
2021-03-20 13:45:02 -0400

    added level common prefab

---
+++ Assets/Scripts/FollowerMover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowerMover : MonoBehaviour
+{
+    public Transform reference;
+
+    private Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - reference.position;
+    }
+
+    void Update()
+    {
+        transform.position = reference.position + offset;   
+    }
+}
