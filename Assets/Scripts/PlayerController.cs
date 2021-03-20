cc36079b02f066c6009bd91601593ce716b6326e
schaub818 <schaub818@gmail.com>
2021-03-20 11:01:04 -0400

    Started creating PlayerController script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    new Rigidbody rigidbody;
+
+    public float movementSpeed = 500.0f;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetVelocity = new Vector3(Input.GetAxisRaw("Horizontal"), 0.0f, Input.GetAxisRaw("Vertical"));
+
+        Move(targetVelocity);
+    }
+
+    private void Move(Vector3 targetVelocity)
+    {
+        rigidbody.velocity = (targetVelocity * movementSpeed) * Time.deltaTime;
+    }
+}
