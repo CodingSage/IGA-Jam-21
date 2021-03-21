165dc711582807d816aaddc90c77f34285ad5046
Partymongrel <kyaldo_is_stu@hotmail.com>
2021-03-21 03:12:12 -0600

    Added audio and audioReadme

---
+++ Assets/Audio/AUDIO_README.cs
@@ -0,0 +1,10 @@
+﻿//Both music files loop continuously
+
+//SFX is labeled generically, and can be mixed/matched how you see fit. You don't even have to use all of it!
+
+//Bonks are for things hitting a wall (player and objects)
+//ObjectPush and PlayerWalk both loop, so as long as an object is moving/player is walking the sound can play
+//PlayerGrab_Release is the sound for when the character grabs AND releases an object
+//UI_Hover is for when the mouse hovers over a button
+
+//This is all I can do for now. There is a chance that I can get home early from work but I misread the times and so most likely this will be it for sound on my end. SOrry that I didn't get to a happy version of the song!
