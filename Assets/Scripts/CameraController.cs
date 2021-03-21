7f1e22e2fce0c55582c8d32eb90565f592409914
schaub818 <schaub818@gmail.com>
2021-03-21 12:14:44 -0400

    Added camera collision

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,16 +9,22 @@ public class CameraController : MonoBehaviour
     public Vector3 offset;
 
     private Vector3 cameraSpeed;
+    private Rigidbody rb;
 
     private void Start()
     {
-        transform.position = target.position + offset;
+        rb = GetComponent<Rigidbody>();
+        //transform.position = target.position + offset;
+        rb.MovePosition(target.position + offset);
     }
 
-    void LateUpdate()
+    void FixedUpdate()
     {
         Vector3 desiredPosition = target.position + offset;
+        Vector3 newPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref cameraSpeed, smoothSpeed);
 
-        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref cameraSpeed, smoothSpeed);
+        //transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref cameraSpeed, smoothSpeed);
+
+        rb.MovePosition(newPosition);
     }
 }
